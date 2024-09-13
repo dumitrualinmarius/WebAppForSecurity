a1ad16e6794bd0785ef194b3e81825c8aa200635
Dumitru Alin-Marius <alin.dumitru@telus.com>
2024-09-13 10:07:49 +0300

    git ignore

--- WebAppForSecurity/obj/Debug/net8.0/WebAppForSecurity.AssemblyInfo.cs
+++ WebAppForSecurity/obj/Debug/net8.0/WebAppForSecurity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppForSecurity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11dd26d591f83124932b001346ad594e9a2a6d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppForSecurity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppForSecurity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
