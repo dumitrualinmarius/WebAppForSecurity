d03bdde27c0bd440d1ef0d168978b3ed4b8d3dd5
Dumitru Alin-Marius <alin.dumitru@telus.com>
2024-09-18 17:42:27 +0300

    add login and logout partial view

---
+++ WebAppForSecurity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Security.Claims;
+
+namespace WebAppForSecurity.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await HttpContext.SignOutAsync("MyCookieAuth");
+            return RedirectToAction("/Index");
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
