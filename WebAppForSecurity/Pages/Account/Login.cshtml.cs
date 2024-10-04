c1df9da3a7560b45cb07047b17226d50a4bc7123
Dumitru Alin-Marius <alin.dumitru@telus.com>
2024-10-04 11:56:47 +0300

    Consume the endpoint using JWT token

--- WebAppForSecurity/Pages/Account/Login.cshtml.cs
+++ WebAppForSecurity/Pages/Account/Login.cshtml.cs
@@ -1,9 +1,8 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
-using System.Net;
 using System.Security.Claims;
+using WebAppForSecurity.Authorization;
 
 namespace WebAppForSecurity.Pages
 {
@@ -49,17 +48,5 @@ namespace WebAppForSecurity.Pages
 
             return Page();
         }
-    }
-
-    public class Credential
-    {
-        [Required]
-        [Display(Name = "User Name")]
-        public string UserName { get; set; } = string.Empty;
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; } = string.Empty;
-        [Display(Name = "Remember Me")]
-        public bool RememberMe { get; set; }
-    }
+    }    
 }
--- WebAppForSecurity/Pages/HRManager.cshtml.cs
+++ WebAppForSecurity/Pages/HRManager.cshtml.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+using WebAppForSecurity.Authorization;
 using WebAppForSecurity.DTO;
 
 namespace WebAppForSecurity.Pages
@@ -21,6 +25,13 @@ namespace WebAppForSecurity.Pages
         public async Task OnGetAsync()
         {
             var httpClient = httpClientFactory.CreateClient("OurWebAPI");
+
+            var res = await httpClient.PostAsJsonAsync("auth", new Credential { UserName = "admin", Password = "password" });
+            res.EnsureSuccessStatusCode();
+            string strJwt = await res.Content.ReadAsStringAsync();
+            var token = JsonConvert.DeserializeObject<JwtToken>(strJwt);
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token?.AccessToken ?? string.Empty);
+
             weatherForecast = 
                 await httpClient.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast")
                 ?? new List<WeatherForecastDTO>();
